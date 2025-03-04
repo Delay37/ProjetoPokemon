111583cf72cccf6589fa59a9482bb5b072ca8146
Delay37 <benettiescritorio@gmail.com>
2025-03-04 20:26:27 -0300

    Finaliza o exercicio, corrige bugs no script

--- script/script.js
+++ script/script.js
@@ -1,20 +1,66 @@
 async function BuscarPokemon() {
-    // buscar value de numPokemon
+
     let url = "https://pokeapi.co/api/v2/pokemon/";
     numPokemonjs = document.getElementById("numPokemon").value;
 
-    // buscar na API o Pokémon correspondente
     url = url + numPokemonjs;
-    let response = await fetch(url); // Aguarda a resposta da API
-    let respostaApi = await response.json(); // Converte para JSON corretamente
+    let response = await fetch(url);  
 
-    // adicionar imagem na div pokeImagem
+    if (response.status === 404) {
+        alert('Pokemon não encontrado');
+    } else {
+        let respostaApi = await response.json();
+    
+
     console.log(respostaApi);
     document.getElementById("pokeImagem").src = respostaApi.sprites.front_default;
     document.getElementById("pokeImagem").style.display = "block"
-    // adicionar nome em pokeNome 
-    document.getElementById("pokeNome").innerHTML = ("#"+numPokemonjs +" "+ respostaApi.species.name)
-    // mudar a classe de div de tipo do pokemon 
     
-    // remover a classe displayNone
-}
+    document.getElementById("pokeNome").innerHTML = ("#"+numPokemonjs +" "+ respostaApi.species.name)
+
+    const container = document.getElementById("tiposPokemon")
+    const divs = container.querySelectorAll(".pokemon-type")
+    
+    const traducoes = {
+        normal: "Normal",
+        fighting: "Luta",
+        flying: "Voador",
+        poison: "Venenoso",
+        ground: "Terrestre",
+        rock: "Pedra",
+        bug: "Inseto",
+        ghost: "Fantasma",
+        steel: "Aço",
+        fire: "Fogo",
+        water: "Água",
+        grass: "Grama",
+        electric: "Elétrico",
+        psychic: "Psíquico",
+        ice: "Gelo",
+        dragon: "Dragão",
+        dark: "Sombrio",
+        fairy: "Fada",
+      }
+      divs.forEach(div => div.classList.add('displayNone'))
+      divs.forEach(div => {
+        div.classList.forEach(className => {
+          if (className !== "displayNone" && className !== "pokemon-type") {
+            div.classList.remove(className);
+          }
+          div.innerHTML = ''
+        });
+      });
+      
+  
+      for (let i = 0; i < respostaApi.types.length; i++) {
+
+        divs[i].classList.add(respostaApi.types[i].type.name)
+        
+        divs[i].classList.remove("displayNone")
+        
+        const tipoOriginal = respostaApi.types[i].type.name
+        const tipoTraduzido = traducoes[tipoOriginal]
+        
+        divs[i].innerHTML = tipoTraduzido || tipoOriginal
+    }
+}}
